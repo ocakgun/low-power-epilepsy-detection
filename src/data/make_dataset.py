6d62e36e3d794bbc84a22e0f3cc49e0225004046
Joey van Rijn <jmsvanrijn@github.com>
2020-04-21 13:30:51 +0200

    Dataset now correctly exports processed data and removed visualisation code

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -8,6 +8,10 @@ import numpy as np
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 import time
+import wget
+import shutil
+import random
+import string
 
 
 def main():
@@ -16,32 +20,66 @@ def main():
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
     patient_folder = os.getenv("RAW_DATA_FOLDER")
     create_file_lists(patient_folder)
 
 
 def create_file_lists(patient_folder):
     # Select the patients you want to check (available are; 1-24)
-    PATIENTS = range(1, 23)
+    PATIENTS = range(1, 24)
 
     # Make plots of patient
     PLOT = 0
 
-    # seizures_recordings, normal_recordings = list_files(patient_folder, PATIENTS)
-    seizure_timestamps, normal_recording_files = analyse_patients(patient_folder, PATIENTS)
+    #Base url of data
+    physioUrl = "https://physionet.org/files/chbmit/1.0.0/"
 
-    extract_seizure_edf(patient_folder, seizure_timestamps, normal_recording_files)
+    # Download files and process the to raw to processed data, beware cost a lot of time
+    DOWNLOAD_FILES = 0
 
-    # pearson_corr = pearson_correlation(pd_signal)
-    #
-    # # Reshapes to only contain the lowest values
-    # triu_pearson_corr = pearson_corr.mask(np.triu(np.ones(pearson_corr.shape, dtype=np.bool_)))
-    # print(triu_pearson_corr.min(0))
+    if DOWNLOAD_FILES:
+        DOWNLOAD_FILES = yes_or_no_helper()
+
+    if DOWNLOAD_FILES:
+        download_in_cycles(physioUrl)
+    else:
+        seizure_timestamps, normal_recording_files = analyse_patients(patient_folder, PATIENTS)
+        extract_seizure_edf(patient_folder, seizure_timestamps, normal_recording_files)
+
+def yes_or_no_helper():
+    yes = {"yes", "y", "ye", ""}
+    no = {"no", "n"}
+
+    print("Downloading files (50GB) and processing them takes a long time (+1 hour) are you sure? [y/n]")
+    choice = input().lower()
+    if choice in yes:
+        return 1
+    elif choice in no:
+        return 0
+    else:
+        print("Please respond with 'yes' or 'no'")
+        return yes_or_no_helper()
+
+
+def download_in_cycles(baseUrl):
+    raw_folder = "data/raw/"
+    wget.download(baseUrl + "/RECORDS-WITH-SEIZURES", raw_folder)
+    file = open(raw_folder + "RECORDS-WITH-SEIZURES", 'r')
+    print(file.read())
 
 
 def extract_seizure_edf(patient_folder, seizures, normal_files):
     t_general = time.time()
-    print(seizures)
+    t_delay = 60
+
+    if os.path.isdir("data/processed/seizures"):
+        shutil.rmtree("data/processed/seizures", ignore_errors=True)
+        shutil.rmtree("data/processed/normal", ignore_errors=True)
+
+    os.mkdir("data/processed/seizures")
+    os.mkdir("data/processed/normal")
+
     for seizure in seizures:
         signals, signal_headers, header = pyedflib.highlevel.read_edf(patient_folder + seizure[0])
         signals = np.array(signals)
@@ -50,31 +88,26 @@ def extract_seizure_edf(patient_folder, seizures, normal_files):
         start = seizure[1]*sample_rate
         end = seizure[2]*sample_rate
         seizure_signal = signals[:, start: end]
-        normal_signal = signals[:, start+60*sample_rate: end+60*sample_rate]
+        normal_signal = signals[:, start+t_delay*sample_rate: end+t_delay*sample_rate]
+        random_part = random.choice(string.ascii_letters) + random.choice(string.ascii_letters)
 
-        print(seizure)
-        filename = "extract-seizures/" + seizure[0].split("/")[1].split(".")[0] + "-seizure.edf"
-        print(filename)
+        seizure_filename = "data/processed/seizures/" + seizure[0].split("/")[1].split(".")[0] + "_" + random_part + "-seizure.edf"
+        normal_filename = "data/processed/normal/" + seizure[0].split("/")[1].split(".")[0] + "_" + random_part + ".edf"
 
-        pyedflib.highlevel.write_edf(filename, seizure_signal, signal_headers, header)
-        # normal_edf = pyedflib.highlevel.write_edf("edf_normal_file", normal_signal, signal_headers, header)
+        # Some headers are broken, this fixes them
+        for signal_header in signal_headers:
+            if signal_header["digital_min"] == 0:
+                signal_header["digital_min"] = -1
+            if signal_header["digital_max"] == 0:
+                signal_header["digital_max"] = 1
 
-
-        # mne.set_log_level("WARNING")
-        # raw = mne.io.read_raw_edf(patient_folder + seizure[0], preload=True)
-        # print(raw.info["ch_names"])
-
-        # raw.set_montage("standard_1020", match_case=False, verbose=True)
-
-        # raw.plot()
-        # plt.show()
-
-
-        # montage = mne.channels.make_standard_montage("standard_1020")
-        print("Check")
+        pyedflib.highlevel.write_edf(seizure_filename, seizure_signal, signal_headers, header)
+        pyedflib.highlevel.write_edf(normal_filename, normal_signal, signal_headers, header)
 
     elapsed = time.time() - t_general
     print(elapsed)
+
+
 def analyse_patients(ROOT, patient_array):
     seizure_data = []
     normal_data = []
@@ -121,12 +154,7 @@ def clock_to_timestamp(clock_time):
     seconds = int(clock_time[-2:])
     minutes = int(clock_time[-5:-3])
     hours = int(clock_time[-8:-6])
-
     return seconds + minutes*60 + hours*60*60
-
-
-def edf_to_wav(edf_file):
-    signals, signal_headers, header = pyedflib.highlevel.read_edf(edf_file)
 
 
 def load_edf_to_pd(edf_file):
@@ -138,32 +166,6 @@ def load_edf_to_pd(edf_file):
 
     pd_eeg = pd.DataFrame(data=signals, index=pd_signal_headers)
     return pd_eeg
-
-
-def pearson_correlation(pd_eeg):
-    return pd_eeg.T.corr(method="pearson")
-
-
-def list_files(ROOT, patient_array):
-    tot_seizure_recordings = []
-    tot_normal_recordings = []
-
-    for patient in patient_array:
-        patient_folder = "chb0" + str(patient)
-        recordings = os.listdir(ROOT + patient_folder)
-        seizure_recordings: List[str] = []
-        normal_recordings: List[str] = []
-
-        for recording in recordings:
-            if (".edf" and "seizures") in recording:
-                seizure_recordings.append(ROOT + patient_folder + "/" + recording)
-            elif ".edf" in recording:
-                normal_recordings.append(ROOT + patient_folder + "/" + recording)
-
-        tot_normal_recordings.append(normal_recordings)
-        tot_seizure_recordings.append(seizure_recordings)
-
-    return tot_seizure_recordings, tot_normal_recordings
 
 
 if __name__ == '__main__':
