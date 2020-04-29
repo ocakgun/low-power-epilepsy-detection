db65d4772c7df8812abad55ca857e69630826d1f
Joey van Rijn <jmsvanrijn@github.com>
2020-04-29 10:35:41 +0200

    80percent version of the loader

--- src/models/dataloader.py
+++ src/models/dataloader.py
@@ -1,24 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+
 from pathlib import Path
 
-#
-# class EpilepsyData(Dataset):
-#     def __init__(self):
-#         # Get directory listing from path
-#         files = Path(path).glob("*.wav")
-#         print(files)
-#         # Iterate through the listing and create a list of tuples (filename, label)
-        self.items = [(str(f), f.name.split("-")[-1].replace(".wav", "")) for f in files]
-#         self.length = len(self.items)
-#
-#     def __getitem__(self, index):
-#         filename, label = self.items[index]
-#         # Find normal
-#         # audioTensor, rate = torchaudio.load(filename)
-#         return (audioTensor, int(label))
-#
-#     def __len__(self):
-#         return self.length
+class EpilepsyData(Dataset):
+
+    def __init__(self, path):
+        super().__init__()
+        # Get directory listing from path
+        files = sorted(Path(EEG_DATA).glob('**/*.edf'))
+        # Iterate through the listing and create a list of tuples (filename, label)
+        self.items = [(str(f), f.name.split("-")[-1].replace(".edf", "")) for f in files]
+        self.length = len(self.items)
+
+    def __getitem__(self, index):
+        filename, label = self.items[index]
+        # audioTensor, rate = torchaudio.load(filename)
+        audioTensor = ""
+        return (audioTensor, int(label))
+
+    def __len__(self):
+        return self.length
+
 
 bs = 12
 EEG_DATA = Path.cwd() / "data/processed/"
+train_epilepsy = EpilepsyData(EEG_DATA)
+print(len(files))
 
+loader = data.DataLoader(train_epilepsy, batch_size=bs, shuffle=True)
